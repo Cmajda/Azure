79ecd6fe8de00bcaeb436cffc54cb8dd4c518937
cmajda <cmajdalkam@gmail.com>
2021-01-22 02:06:18 +0100

    edited postinstall script

--- ARM_Template/OnboardingMDE/Scripts/postinstall.ps1
+++ ARM_Template/OnboardingMDE/Scripts/postinstall.ps1
@@ -1,3 +1,4 @@
 New-Item -Path C:\temp -ItemType Directory -Force
 
-Invoke-WebRequest https://raw.githubusercontent.com/Cmajda/Azure/master/ARM_Template/Temporaly/UI/allelement.json -outfile c:\temp\01.json
+Invoke-WebRequest https://raw.githubusercontent.com/Cmajda/Azure/master/ARM_Template/OnboardingMDE/Scripts/WindowsDefenderATPLocalOnboardingScript.cmd -outfile c:\temp\WindowsDefenderATPLocalOnboardingScript.cmd
+Start-Process c:\temp\WindowsDefenderATPLocalOnboardingScript.cmd -Wait
